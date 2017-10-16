b470f9dd60b4b4f8fe9637c126e6a9627536bee4
abalone0204 <abalone0204@gmail.com>
2017-10-16 14:10:53 +0800

    Add loading

--- layouts/Main.js
+++ layouts/Main.js
@@ -3,9 +3,14 @@ import Router from 'next/router'
 import Head from 'next/head'
 import HeroList from '../components/HeroList'
 
+Router.onRouteChangeStart = (url) => {
+	NProgress.start()
+}
+Router.onRouteChangeComplete = () => NProgress.done()
+Router.onRouteChangeError = () => NProgress.done()
 
 const Main = (props) => (
-	<div>
+	<div style={{ marginBottom: 20 }}>
 		<Head>
 			{/* Import CSS for nprogress */}
 			<link rel='stylesheet' type='text/css' href='/static/ngprogress.css' />
